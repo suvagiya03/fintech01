d82ad825e04e6f887aa676905929eafbd55429a3
suvagiya03 <suvagiyadeep03@gmail.com>
2023-06-27 08:48:09 +0530

    first commit

---
+++ 1.py
@@ -0,0 +1,11 @@
+class Bird:
+    def fly(self):
+        print("bird flying")
+class Crow(Bird):
+    def cawing(self):
+        print("crow cawing")
+        
+ob= Crow()
+ob.fly()
+ob.cawing()
+print("we are testing how to get back data from git")
